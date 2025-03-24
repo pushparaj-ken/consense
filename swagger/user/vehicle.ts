5e2faa2f4936caeb5d6bde3af32912b3764dc9b3
pushparaj7396 <pushparaj7396@gmail.com>
2025-03-24 09:49:23 +0530

    completed

--- swagger/user/vehicle.ts
+++ swagger/user/vehicle.ts
@@ -11,6 +11,27 @@
  *   get:
  *     summary: Vehicle List
  *     tags: [Vehicle]
+ *     parameters:
+ *       - in: query
+ *         name: VEHICLE_ID
+ *         schema:
+ *           type: string
+ *         required: false
+ *         description: Filter Vehicle  by id
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: string
+ *           example: 20
+ *         required: false
+ *         description: No of Records
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: string
+ *           example: 1
+ *         required: false
+ *         description: page No
  *     responses:
  *       200:
  *         description: Success
